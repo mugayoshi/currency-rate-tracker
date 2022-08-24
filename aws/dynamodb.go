110d07672b47c9c9d7e9b32189225ae0bf231226
mugayoshi <muga.y.j3039@gmail.com>
2022-08-24 14:44:34 +0200

    Added access to DB

---
+++ aws/dynamodb.go
@@ -0,0 +1,119 @@
+package aws
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/mugayoshi/currency_rate_tracker/helpers"
+)
+
+type CurrencyRateData struct {
+	Date string  `dynamodbav:"date"`
+	Rate float32 `dynamodbav:"rate"`
+	Base string  `dynamodbav:"base"`
+}
+type CurrencyData struct {
+	LastData CurrencyRateData `dynamodbav:"last-data"`
+	Minimum  CurrencyRateData `dynamodbav:"minimum"`
+}
+
+type ResponseJson struct {
+	JsonData CurrencyData `dynamodbav:"json-data"`
+}
+
+const TABLE_NAME = "currency-rate"
+
+func GetDynamoDbClient() *dynamodb.Client {
+	accessKey := helpers.GetEnvVariable("DYNAMO_API")
+	secret := helpers.GetEnvVariable("DYNAMO_SECRET")
+	region := helpers.GetEnvVariable("DYNAMO_REGION")
+	credentials := credentials.NewStaticCredentialsProvider(accessKey, secret, "")
+	client := dynamodb.NewFromConfig(aws.Config{
+		Region:      region,
+		Credentials: credentials,
+	})
+	return client
+}
+
+func GetCurrencyItem(client *dynamodb.Client, currency string) (CurrencyData, error) {
+	item, err := client.GetItem(context.TODO(), &dynamodb.GetItemInput{
+		TableName: aws.String(TABLE_NAME),
+		Key: map[string]types.AttributeValue{
+			"name": &types.AttributeValueMemberS{Value: currency},
+		},
+	})
+	if err != nil {
+		var d CurrencyData
+		return d, errors.New("api error")
+	}
+	if item.Item == nil {
+		var d CurrencyData
+		return d, errors.New("not found")
+	}
+	fmt.Printf("%+v\n", item.Item)
+	actual := ResponseJson{}
+	e := attributevalue.UnmarshalMap(item.Item, &actual)
+	if e != nil {
+		var d CurrencyData
+		return d, errors.New("unmarshal error")
+	}
+
+	return actual.JsonData, nil
+}
+
+func updateItemBase(client *dynamodb.Client, input *dynamodb.UpdateItemInput) (bool, error) {
+	_, err := client.UpdateItem(context.TODO(), input)
+	if err != nil {
+		fmt.Printf("%+v\n", err)
+		return false, errors.New("update api error")
+	}
+	return true, nil
+}
+
+func UpdateLastData(client *dynamodb.Client, currency string, rate float32, date string) (bool, error) {
+	updateInput := &dynamodb.UpdateItemInput{
+		TableName: aws.String(TABLE_NAME),
+		Key: map[string]types.AttributeValue{
+			"name": &types.AttributeValueMemberS{Value: currency},
+		},
+		UpdateExpression: aws.String("SET #json.#last.#rate = :rate, #json.#last.#date = :date"),
+		ExpressionAttributeNames: map[string]string{
+			"#json": "json-data",
+			"#last": "last-data",
+			"#rate": "rate",
+			"#date": "date",
+		},
+		ExpressionAttributeValues: map[string]types.AttributeValue{
+			":rate": &types.AttributeValueMemberN{Value: fmt.Sprintf("%f", rate)},
+			":date": &types.AttributeValueMemberS{Value: date},
+		},
+	}
+	return updateItemBase(client, updateInput)
+}
+
+func UpdateMinimumRate(client *dynamodb.Client, currency string, rate float32, date string) (bool, error) {
+	updateInput := &dynamodb.UpdateItemInput{
+		TableName: aws.String(TABLE_NAME),
+		Key: map[string]types.AttributeValue{
+			"name": &types.AttributeValueMemberS{Value: currency},
+		},
+		UpdateExpression: aws.String("SET #json.#min.#rate = :rate, #json.#min.#date = :date"),
+		ExpressionAttributeNames: map[string]string{
+			"#json": "json-data",
+			"#min":  "minimum",
+			"#rate": "rate",
+			"#date": "date",
+		},
+		ExpressionAttributeValues: map[string]types.AttributeValue{
+			":rate": &types.AttributeValueMemberN{Value: fmt.Sprintf("%f", rate)},
+			":date": &types.AttributeValueMemberS{Value: date},
+		},
+	}
+	return updateItemBase(client, updateInput)
+}
--- currency/fiat/cmd.go
+++ currency/fiat/cmd.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/mugayoshi/currency_rate_tracker/aws"
 	"github.com/mugayoshi/currency_rate_tracker/constants"
 	"github.com/mugayoshi/currency_rate_tracker/helpers"
 	"github.com/mugayoshi/currency_rate_tracker/slack"
@@ -18,6 +20,30 @@ func createCurrencyFluctuationNotificationMessage(base string, target string, st
 	return fmt.Sprintf("%s\n%s\n%s\n%s", firstLine, secondLine, thirdLine, fourthLine)
 }
 
+func sendUpdateToSlack(startDate, endDate, targetCurrency, baseCurrency string, dbClient *dynamodb.Client) {
+	change := getFluctuationBaseJpy(startDate, endDate, targetCurrency)
+	text := createCurrencyFluctuationNotificationMessage(targetCurrency, baseCurrency, startDate, endDate, change)
+	currentItem, getErr := aws.GetCurrencyItem(dbClient, targetCurrency)
+	if getErr != nil {
+		fmt.Println("couldn't get the last data")
+		return
+	}
+
+	isMinimumRate := currentItem.Minimum.Rate > change.EndRate
+	isSuccess, updateErr := aws.UpdateLastData(dbClient, targetCurrency, change.EndRate, endDate)
+	if updateErr != nil || !isSuccess {
+		fmt.Println("couldn't update the last data")
+		return
+	}
+	if !isMinimumRate {
+		slack.SendMessageToMoneyChannel(text)
+		return
+	}
+	message := fmt.Sprintf("%s\n!!Minimum rate: %f!!", text, change.EndRate)
+	slack.SendMessageToMoneyChannel(message)
+	aws.UpdateMinimumRate(dbClient, targetCurrency, change.EndRate, endDate)
+}
+
 func SendCurrencyUpdates() {
 	now := time.Now()
 
@@ -25,16 +51,10 @@ func SendCurrencyUpdates() {
 	endDate := helpers.GetYyyyMmDd(now)
 
 	fiat := constants.FIAT_CURRENCY
-	eur := fiat.Eur
 	jpy := fiat.Jpy
-	usd := fiat.Usd
-	fmt.Println("EUR/JPY")
-	changeEurJpy := getFluctuationBaseJpy(startDate, endDate, eur)
-	textEurJpy := createCurrencyFluctuationNotificationMessage(eur, jpy, startDate, endDate, changeEurJpy)
-	slack.SendMessageToMoneyChannel(textEurJpy)
-
-	fmt.Println("USD/JPY")
-	changeUsdJpy := getFluctuationBaseJpy(startDate, endDate, usd)
-	textUsdJpy := createCurrencyFluctuationNotificationMessage(usd, jpy, startDate, endDate, changeUsdJpy)
-	slack.SendMessageToMoneyChannel(textUsdJpy)
+	dbClient := aws.GetDynamoDbClient()
+	targetCurrencies := []string{fiat.Eur, fiat.Usd}
+	for _, c := range targetCurrencies {
+		sendUpdateToSlack(startDate, endDate, c, jpy, dbClient)
+	}
 }
